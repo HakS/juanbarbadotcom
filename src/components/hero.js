5cf60a5d656ec02d4448862815813361d8b6df88
Juan Barba <juan.barba.o@gmail.com>
2020-10-07 21:27:31 -0500

    Styled hero with tailwind

--- src/components/hero.js
+++ src/components/hero.js
@@ -20,20 +20,14 @@ export default function Hero() {
     <>
       <div tw="relative bg-gray-100 mx-auto flex items-center w-full h-screen justify-center">
         <div>
-          <div tw="flex flex-wrap items-center justify-center">
+          <div tw="mt-16 md:mt-32 flex flex-wrap items-center justify-center">
             <Img tw="w-auto" fixed={data.file.childImageSharp.fixed} alt="Juan Barba" />
             <div tw="w-full mt-8 md:mt-0 md:w-auto text-center md:text-left md:ml-4">
               <div tw="ml-1 mt-0 text-2xl">Hello! My name is</div>
               <h1 tw="mt-0 mb-2 text-5xl sm:text-6xl">Juan Barba</h1>
             </div>
           </div>
-          <h2 css={{
-            lineHeight: '2.2rem',
-            marginTop: '3.1rem',
-            paddingTop: '1.7rem',
-            borderTop: '1px solid #ececec',
-            textAlign: 'center',
-          }} tw="mb-0">
+          <h2 tw="leading-9 mt-10 md:mt-12 p-5 md:p-6 text-center mb-0 border-t border-gray-300">
             I'm a full-stack Web Developer
             <br/>
             remotely available based in UTC-5
