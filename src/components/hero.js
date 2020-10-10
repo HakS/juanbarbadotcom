972fcd6ec9be4546e235cb6e0be492e04c2e88aa
Juan Barba <juan.barba.o@gmail.com>
2020-10-10 12:02:01 -0500

    Finished social networks buttons, styled all hero

--- src/components/hero.js
+++ src/components/hero.js
@@ -22,26 +22,30 @@ export default function Hero() {
 
   return (
     <>
-      <div tw="relative bg-gray-100 mx-auto flex items-center w-full h-screen justify-center">
+      <div tw="relative text-gray-700 font-light bg-gray-100 mx-auto flex items-center w-full h-screen justify-center">
         <div>
           <div tw="mt-16 md:mt-32 flex flex-wrap items-center justify-center">
             <Img tw="w-auto" fixed={data.file.childImageSharp.fixed} alt="Juan Barba" />
             <div tw="w-full mt-8 md:mt-0 md:w-auto text-center md:text-left md:ml-4">
               <div tw="ml-1 mt-0 text-2xl">Hello! My name is</div>
-              <h1 tw="mt-0 mb-2 text-5xl sm:text-6xl">Juan Barba</h1>
+              <h1 tw="text-gray-700 font-light tracking-tight mt-0 mb-2 text-5xl sm:text-6xl">Juan Barba</h1>
             </div>
           </div>
-          <h2 tw="leading-9 mt-10 md:mt-12 p-5 md:p-6 text-center mb-0 border-t border-gray-300">
+          <h2 tw="text-gray-700 font-light leading-9 mt-10 md:mt-12 p-5 md:p-6 text-center mb-0 border-t border-gray-300">
             I'm a full-stack Web Developer
             <br/>
             remotely available based in UTC-5
           </h2>
+          <div tw="md:absolute text-center bottom-0 right-0 mb-5 mr-5">
+            <a target="_blank" tw="mr-4 text-gray-500" href="mailto:juan.barba.o@gmail.com">
+              <FontAwesomeIcon tw="fill-current" icon={faEnvelope} size="3x" />
+            </a>
+            <a target="_blank" tw="text-gray-500" href="https://www.linkedin.com/in/jbarba109/">
+              <FontAwesomeIcon tw="fill-current" icon={faLinkedin} size="3x" />
+            </a>
+          </div>
         </div>
 
-        <div tw="absolute bottom-0 right-0 mb-5 mr-5">
-          <FontAwesomeIcon icon={faEnvelope} size="3x" />
-          <FontAwesomeIcon icon={faLinkedin} size="3x" />
-        </div>
       </div>
     </>
   )
