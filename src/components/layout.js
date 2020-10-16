5742e42859ac45814b27a40272a6ae98d8a81887
Juan Barba <juan.barba.o@gmail.com>
2020-10-16 09:45:40 -0500

    Improved typography for mobile devices

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,13 +3,33 @@ import { Helmet } from "react-helmet";
 import { GlobalStyles } from "twin.macro";
 import Hero from './hero';
 import { TypographyStyle, GoogleFont } from 'react-typography';
+import { MOBILE_MEDIA_QUERY } from "typography-breakpoint-constants"
 
 import Typography from 'typography';
 import moragaTheme from 'typography-theme-moraga';
 import { graphql, useStaticQuery } from "gatsby";
 import Zone from "./zone";
+import verticalRhythm from "compass-vertical-rhythm";
 
-const typography = new Typography(moragaTheme)
+const theme = {...moragaTheme};
+theme.baseFontSize = '22px';
+theme.includeNormalize = false;
+theme.headerWeight = 300;
+theme.overrideStyles = ({ adjustFontSizeTo, scale, rhythm }, options) => {
+  const vr = verticalRhythm({
+    baseFontSize: "19px",
+    baseLineHeight: "30.65px",
+  })
+  return {
+    [MOBILE_MEDIA_QUERY]: {
+      html: {
+        ...vr.establishBaseline(),
+      },
+    },
+  }
+};
+
+const typography = new Typography(theme);
 
 const Layout = ({ children }) => {
 
